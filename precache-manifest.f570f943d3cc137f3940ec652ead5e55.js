ea6236b352f586ca5daf820a8de636107767aaba
bhagyasri <baagisri@gmail.com>
2020-09-17 08:50:32 +0530

    Updates

---
+++ precache-manifest.f570f943d3cc137f3940ec652ead5e55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1471307c929d2088783b3d51f50baee",
+    "url": "/usersApp/index.html"
+  },
+  {
+    "revision": "f7731c5546aafd508c5d",
+    "url": "/usersApp/static/css/2.5d139a3f.chunk.css"
+  },
+  {
+    "revision": "847817818eb3b8b5ce20",
+    "url": "/usersApp/static/css/main.617aa86a.chunk.css"
+  },
+  {
+    "revision": "f7731c5546aafd508c5d",
+    "url": "/usersApp/static/js/2.6aeb218f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/usersApp/static/js/2.6aeb218f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "847817818eb3b8b5ce20",
+    "url": "/usersApp/static/js/main.06bbf032.chunk.js"
+  },
+  {
+    "revision": "bac7cac477dfaff3e409",
+    "url": "/usersApp/static/js/runtime-main.555362a7.js"
+  }
+]);
